85cc62abf5ce701857dd77b723c2ac034e936765
Oliver <ofcharrison@gmail.com>
2020-01-24 13:10:04 +0000

    Java file added, used to call python file, wait & listen, then execute commands based on what is said

--- MicrophoneSpeech.py
+++ MicrophoneSpeech.py
@@ -5,8 +5,9 @@ print(s_r.__version__) # just to print the version not required
 r = s_r.Recognizer()
 device_list = s_r.Microphone.list_microphone_names()
 print(s_r.Microphone.list_microphone_names())
-device_position = s_r.Microphone.list_microphone_names().index('AK5371: USB Audio (hw:1,0)')
-my_mic = s_r.Microphone(device_index=device_position) #my device index is 0, you have to put your device index
+#USB_MICROPHONE_POSITION = s_r.Microphone.list_microphone_names().index('AK5371: USB Audio (hw:1,0)')
+MAC_POSITION = 0
+my_mic = s_r.Microphone(device_index=MAC_POSITION) #my device index is 0, you have to put your device index
 with my_mic as source:
     print("Say now!!!!")
     r.adjust_for_ambient_noise(source) #reduce noise
--- src/AIController.java
+++ src/AIController.java
@@ -1,27 +1,69 @@
-import sun.misc.IOUtils;
-
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileReader;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.Calendar;
 import java.util.Scanner;
 
 public class AIController {
 
         public static void main(String args[]) throws Exception{
-//            Runtime runtime = Runtime.getRuntime();
-//            Process process = runtime.exec("python /Users/oliverharrison/Projects/side_projects/FakeAI/MicrophoneSpeech.py");
+
+            Calendar c = Calendar.getInstance();
+            int timeOfDay = c.get(Calendar.HOUR_OF_DAY);
+            if (timeOfDay<12){System.out.println("morning");}
+            else{System.out.println("evening");}
+
+
+            Runtime runtime = Runtime.getRuntime();
+            Process process = runtime.exec("python /Users/oliverharrison/Projects/side_projects/FakeAI/MicrophoneSpeech.py");
+            Thread.sleep(3000);
+            System.out.println("Pi listening...");
+            Thread.sleep(10000);
+
+
+
 
             String fileName = "/Users/oliverharrison/Projects/side_projects/FakeAI/SpeechDetection.txt";
             File file = new File(fileName);
             FileReader fr = new FileReader(file);
             BufferedReader br = new BufferedReader(fr);
             String line;
-            while((line = br.readLine()) != null){
+
+            while((line = br.readLine()) != null) {
                 //process the line
                 System.out.println(line);
-//              Runtime runtime = Runtime.getRuntime();
-                if(line.contains("youtube")){runtime.exec("open https://youtube.com");}
+                line = line.toLowerCase();
+                System.out.println(line);
+
+                if (line.contains("hey pi") || line.contains("hey pie") || line.contains("hey pai") || line.contains("hey bye") || line.contains("hey bi") || line.contains("hey die") || line.contains("hey dye") ) {
+
+                    if (line.contains("youtube")) {
+                        runtime.exec("open https://youtube.com");
+                    }
+                    if (line.contains("bbc")) {
+                        runtime.exec("open https://bbc.com");
+                    }
+                    if (line.contains("facebook")) {
+                        runtime.exec("open https://facebook.com");
+                    }
+                    if (line.contains("gmail")) {
+                        runtime.exec("open https://gmail.com");
+                    }
+                    if (line.contains("internet") || line.contains("google")) {
+                        runtime.exec("open https://google.com");
+                    }
+                    if (line.contains("amazon")) {
+                        runtime.exec("open https://amazon.co.uk");
+                    }
+                    if (line.contains("netflix")) {
+                        runtime.exec("open https://netflix.co.uk");
+                    }
+
+                }
+                else{System.out.println("You talking to Pi?!");}
             }
 
         }

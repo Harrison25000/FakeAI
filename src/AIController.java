77c6d72b94e45e6d4afbda215bd38b0d4b64cecc
Harrison25000 <ofcharrison@gmail.com>
2020-01-24 16:35:51 +0000

    speech & command execution working on raspberry Pi 3

--- src/AIController.java
+++ src/AIController.java
@@ -6,8 +6,12 @@ import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.Calendar;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class AIController {
+
+	static String macOpen = "open ";
+	static String rasOpen = "xdg-open ";
 
         public static void main(String args[]) throws Exception{
 
@@ -18,7 +22,7 @@ public class AIController {
 
 
             Runtime runtime = Runtime.getRuntime();
-            Process process = runtime.exec("python /Users/oliverharrison/Projects/side_projects/FakeAI/MicrophoneSpeech.py");
+            Process process = runtime.exec("python /home/pi/Projects/FakeAI/MicrophoneSpeech.py");
             Thread.sleep(3000);
             System.out.println("Pi listening...");
             Thread.sleep(10000);
@@ -26,40 +30,83 @@ public class AIController {
 
 
 
-            String fileName = "/Users/oliverharrison/Projects/side_projects/FakeAI/SpeechDetection.txt";
+            String fileName = "/home/pi/Projects/FakeAI/SpeechDetection.txt";
             File file = new File(fileName);
             FileReader fr = new FileReader(file);
             BufferedReader br = new BufferedReader(fr);
             String line;
+	    boolean percentage = false;
 
             while((line = br.readLine()) != null) {
                 //process the line
                 System.out.println(line);
                 line = line.toLowerCase();
                 System.out.println(line);
+		String[] lineArray = line.split(" ");
+		ArrayList<Integer> numbers = new ArrayList<Integer>();
+		for(String x : lineArray){
+			if(x.contains("%")){
+					    System.out.println("percentage sign included"); 
+					    percentage = true;
+					    x=x.replaceAll("%","");
+			}
+			System.out.println(x);
+			if(x.contains("0") || x.contains("1") || x.contains("2") || x.contains("3") || x.contains("4") || x.contains("5") || x.contains("6") || x.contains("7") || x.contains("8") || x.contains("9")){
+					Integer forArray = Integer.parseInt(x);
+					numbers.add(forArray);
+					System.out.println(numbers);
+			}
+		}
 
                 if (line.contains("hey pi") || line.contains("hey pie") || line.contains("hey pai") || line.contains("hey bye") || line.contains("hey bi") || line.contains("hey die") || line.contains("hey dye") ) {
 
                     if (line.contains("youtube")) {
-                        runtime.exec("open https://youtube.com");
+                        runtime.exec(rasOpen + "https://youtube.com");
                     }
                     if (line.contains("bbc")) {
-                        runtime.exec("open https://bbc.com");
+                        runtime.exec(rasOpen + "https://bbc.com");
                     }
                     if (line.contains("facebook")) {
-                        runtime.exec("open https://facebook.com");
+                        runtime.exec(rasOpen + "https://facebook.com");
                     }
                     if (line.contains("gmail")) {
-                        runtime.exec("open https://gmail.com");
+                        runtime.exec(rasOpen + "https://gmail.com");
                     }
                     if (line.contains("internet") || line.contains("google")) {
-                        runtime.exec("open https://google.com");
+                        runtime.exec(rasOpen + "https://google.com");
                     }
                     if (line.contains("amazon")) {
-                        runtime.exec("open https://amazon.co.uk");
+                        runtime.exec(rasOpen + "https://amazon.co.uk");
                     }
                     if (line.contains("netflix")) {
-                        runtime.exec("open https://netflix.co.uk");
+                        runtime.exec(rasOpen + "https://netflix.co.uk");
+                    }
+		    if (line.contains("calculate")) {
+                        if (line.contains(" x ") || line.contains("multiply")){
+				System.out.println("multiplication");
+				System.out.println(numbers.get(0)*numbers.get(1));			
+			}
+			if (line.contains(" + ") || line.contains("plus") || line.contains("add")){
+				System.out.println("addition");
+				System.out.println(numbers.get(0) + numbers.get(1));			
+			}
+			if (line.contains(" - ") || line.contains("minus") || line.contains("subtract") || line.contains("subtracted")){
+				System.out.println("subtraction");
+				System.out.println(numbers.get(0) - numbers.get(1));			
+			}
+			if (line.contains(" / ") || line.contains("divide") || line.contains("divided")){
+				System.out.println("division");
+				System.out.println(numbers.get(0)/numbers.get(1));			
+			}
+			if (percentage==true || line.contains("percent")){
+				System.out.println("percentage");
+				float f = numbers.get(0);
+				double getPercentage = f/100;
+				double totalPercentage = numbers.get(1)*getPercentage;
+				double roundedTotalPercentage = Math.round(totalPercentage*100.0)/100.0;
+				System.out.println(roundedTotalPercentage);
+						
+			}
                     }
 
                 }
